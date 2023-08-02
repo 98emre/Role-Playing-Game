57d174b0b2f07ddeb8df002bdf2eb380374c6b3d
98emre <emre-98@live.se>
2023-08-02 16:05:22 +0200

    feat: added Swashbuckler class with attributes and testet it in main app

--- assignment1/src/main/java/app/App.java
+++ assignment1/src/main/java/app/App.java
@@ -3,10 +3,12 @@ package app;
 import app.heroes.Archer;
 import app.heroes.Barbarian;
 import app.heroes.Hero;
+import app.heroes.Swashbuckler;
 import app.heroes.Wizard;
 
 public class App {
     public static void main(String[] args) {
+        System.out.println();
 
         Hero wizard = new Wizard("Wizard");
         wizard.display();
@@ -23,6 +25,11 @@ public class App {
         Hero barbarian = new Barbarian("Barbarian");
         barbarian.levelUp();
         barbarian.display();
+        System.out.println();
+
+        Hero swashbuckler = new Swashbuckler("Swashbuckler");
+        swashbuckler.levelUp();
+        swashbuckler.display();
         System.out.println();
     }
 }
--- assignment1/src/main/java/app/heroes/Swashbuckler.java
+++ assignment1/src/main/java/app/heroes/Swashbuckler.java
@@ -1,5 +1,31 @@
 package app.heroes;
 
-public class Swashbuckler {
+import java.util.ArrayList;
+
+import app.heroAttributes.HeroAttributes;
+
+public class Swashbuckler extends Hero {
+
+    public Swashbuckler(String name) {
+        super(name);
+
+        this.level = 1;
+        this.validArmorTypes = new ArrayList<>();
+        this.validWeaponTypes = new ArrayList<>();
+
+        this.levelAttributes = new HeroAttributes(2, 6, 1);
+    }
+
+    @Override
+    public void levelUp() {
+        this.level += 1;
+        this.levelAttributes.increaseAttributes(1, 4, 1);
+    }
+
+    @Override
+    public void damage(int damage) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'damage'");
+    }
 
 }
