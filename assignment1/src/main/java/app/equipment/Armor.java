541a29d9b159c406822cfc4bf8ae83bc7667b43c
98emre <emre-98@live.se>
2023-08-02 16:26:51 +0200

    feat: added abstrakt item class to Armor and Weapon java file

--- assignment1/src/main/java/app/equipment/Armor.java
+++ assignment1/src/main/java/app/equipment/Armor.java
@@ -1,5 +1,33 @@
 package app.equipment;
 
-public class Armor {
+import app.Items.ArmorType;
+import app.Items.Item;
+import app.Items.Slot;
+import app.heroAttributes.HeroAttributes;
+import app.heroes.Hero;
+
+public class Armor extends Item {
+
+    private ArmorType armorType;
+    private HeroAttributes armorAttribute;
+
+    public Armor(String name, int requiredLevel, ArmorType armorType, HeroAttributes armorAttribute) {
+        super(name, requiredLevel, getArmorSlot(armorType));
+        this.armorType = armorType;
+        this.armorAttribute = armorAttribute;
+    }
+
+    @Override
+    public void checkHeroCanEquip(Hero hero) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'checkHeroCanEquip'");
+    }
+
+    private static Slot getArmorSlot(ArmorType type) {
+        // Implement the logic to map armor type to the corresponding slot
+        // e.g., CLOTH -> Slot.BODY, LEATHER -> Slot.BODY, etc.
+
+        return null;
+    }
 
 }
--- assignment1/src/main/java/app/equipment/Weapon.java
+++ assignment1/src/main/java/app/equipment/Weapon.java
@@ -1,5 +1,25 @@
 package app.equipment;
 
-public class Weapon {
+import app.Items.Item;
+import app.Items.Slot;
+import app.Items.WeaponType;
+import app.heroes.Hero;
+
+public class Weapon extends Item {
+
+    private WeaponType weaponType;
+    private int weaponDamage;
+
+    public Weapon(String name, int requiredLevel, WeaponType weaponType, int weaponDamage) {
+        super(name, requiredLevel, Slot.WEAPON);
+        this.weaponType = weaponType;
+        this.weaponDamage = weaponDamage;
+    }
+
+    @Override
+    public void checkHeroCanEquip(Hero hero) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'checkHeroCanEquip'");
+    }
 
 }
