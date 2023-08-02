29ba2cf23389c8abd872aa607ec266e984c1e8f6
98emre <emre-98@live.se>
2023-08-02 16:02:41 +0200

    feat: added attribut for barabarian and tested the class in main

--- assignment1/src/main/java/app/App.java
+++ assignment1/src/main/java/app/App.java
@@ -1,6 +1,7 @@
 package app;
 
 import app.heroes.Archer;
+import app.heroes.Barbarian;
 import app.heroes.Hero;
 import app.heroes.Wizard;
 
@@ -9,10 +10,19 @@ public class App {
 
         Hero wizard = new Wizard("Wizard");
         wizard.display();
+        wizard.levelUp();
         System.out.println();
 
         Hero archer = new Archer("Archer");
+        archer.levelUp();
+        archer.levelUp();
+        archer.levelUp();
         archer.display();
+        System.out.println();
+
+        Hero barbarian = new Barbarian("Barbarian");
+        barbarian.levelUp();
+        barbarian.display();
         System.out.println();
     }
 }
--- assignment1/src/main/java/app/heroes/Barbarian.java
+++ assignment1/src/main/java/app/heroes/Barbarian.java
@@ -1,5 +1,30 @@
 package app.heroes;
 
-public class Barbarian {
+import java.util.ArrayList;
+
+import app.heroAttributes.HeroAttributes;
+
+public class Barbarian extends Hero {
+
+    public Barbarian(String name) {
+        super(name);
+        this.level = 1;
+        this.validArmorTypes = new ArrayList<>();
+        this.validWeaponTypes = new ArrayList<>();
+
+        this.levelAttributes = new HeroAttributes(5, 2, 1);
+    }
+
+    @Override
+    public void levelUp() {
+        this.level += 1;
+        this.levelAttributes.increaseAttributes(3, 2, 1);
+    }
+
+    @Override
+    public void damage(int damage) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'damage'");
+    }
 
 }
