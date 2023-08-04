93725cc581b0151f22f6757237d15387fa4cd7d5
98emre <emre-98@live.se>
2023-08-04 10:22:49 +0200

    feat: added another method that handle attributes

--- assignment1/src/main/java/app/heroAttributes/HeroAttribute.java
+++ assignment1/src/main/java/app/heroAttributes/HeroAttribute.java
@@ -17,6 +17,12 @@ public class HeroAttribute {
         this.intelligence += otherAttribute.intelligence;
     }
 
+    public void addAttributes(int strength, int dexterity, int intelligence) {
+        this.strength += strength;
+        this.dexterity += dexterity;
+        this.intelligence += intelligence;
+    }
+
     public int getStrength() {
         return strength;
     }
