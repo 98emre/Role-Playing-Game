083ee39c2ad0bf70f1f3f5cf58ddf881ac12ec6d
98emre <emre-98@live.se>
2023-08-08 16:30:25 +0200

    fix: 1 test of hero

--- assignment1/src/main/java/app/App.java
+++ assignment1/src/main/java/app/App.java
@@ -2,12 +2,8 @@ package app;
 
 import app.heroAttributes.HeroAttribute;
 import app.heroes.Hero;
-import app.heroes.classes.Archer;
-import app.heroes.classes.Barbarian;
-import app.heroes.classes.Swashbuckler;
 import app.heroes.classes.Wizard;
 import app.items.Armor;
-import app.items.Equipment;
 import app.items.Weapon;
 import app.types.ArmorType;
 import app.types.Slot;
@@ -18,22 +14,12 @@ public class App {
         System.out.println();
 
         Weapon weapon1 = new Weapon("Wand", 1, WeaponType.WANDS, 10);
-        Weapon weapon2 = new Weapon("Staff", 3, WeaponType.STAFFS, 20);
-
         Armor armor1 = new Armor("Cloth Helmet", 1, ArmorType.CLOTH, Slot.HEAD, new HeroAttribute(10, 10, 10));
-        Armor armor2 = new Armor("Cloth Boots", 2, ArmorType.CLOTH, Slot.LEGS, new HeroAttribute(20, 20, 20));
-        Armor armor3 = new Armor("Cloth Chest", 2, ArmorType.CLOTH, Slot.BODY, new HeroAttribute(20, 20, 20));
-
         Hero wizard = new Wizard("Emre Merlin");
 
         wizard.equip(weapon1);
         wizard.equip(armor1);
         wizard.levelUp();
-        wizard.equip(armor2);
-        wizard.equip(armor3);
-        wizard.levelUp();
-        wizard.levelUp();
-        wizard.equip(weapon2);
 
         System.out.println(wizard.display());
 
