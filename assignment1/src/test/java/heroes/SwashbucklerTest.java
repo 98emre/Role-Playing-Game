603428f95b3e93e96a5b4957030a2eb9d00ddf80
98emre <emre-98@live.se>
2023-08-09 14:21:33 +0200

    fix: swashbuckler test where each method use just 1 assert

--- assignment1/src/test/java/heroes/SwashbucklerTest.java
+++ assignment1/src/test/java/heroes/SwashbucklerTest.java
@@ -12,18 +12,34 @@ import app.heroes.classes.Swashbuckler;
 public class SwashbucklerTest {
 
     @Test
-    public void testSwashbucklerCreation() {
+    public void test_Swashbuckler_Creation_Check_Strength_Attribute() {
         Hero swashbuckler = new Swashbuckler("Emre Swashbuckler");
         HeroAttribute expectedAttributes = new HeroAttribute(2, 6, 1);
         HeroAttribute actualAttributes = swashbuckler.totalAttributes();
 
         assertEquals(expectedAttributes.getStrength(), actualAttributes.getStrength());
+    }
+
+    @Test
+    public void test_Swashbuckler_Creation_Check_DexterityAttribute() {
+        Hero swashbuckler = new Swashbuckler("Emre Swashbuckler");
+        HeroAttribute expectedAttributes = new HeroAttribute(2, 6, 1);
+        HeroAttribute actualAttributes = swashbuckler.totalAttributes();
+
         assertEquals(expectedAttributes.getDexterity(), actualAttributes.getDexterity());
+    }
+
+    @Test
+    public void test_Swashbuckler_Creation_Check_Intelligence_Attribute() {
+        Hero swashbuckler = new Swashbuckler("Emre Swashbuckler");
+        HeroAttribute expectedAttributes = new HeroAttribute(2, 6, 1);
+        HeroAttribute actualAttributes = swashbuckler.totalAttributes();
+
         assertEquals(expectedAttributes.getIntelligence(), actualAttributes.getIntelligence());
     }
 
     @Test
-    public void testSwashbucklerCorrectDamage() {
+    public void test_Swashbuckler_Correct_Damage() {
         Hero swashbuckler = new Swashbuckler("Emre Swashbuckler");
         double expectedDamage = 1.06;
         double actualDamage = swashbuckler.calculateDamage();
@@ -33,7 +49,7 @@ public class SwashbucklerTest {
     }
 
     @Test
-    public void testSwashbucklerIncreasedLevelCorrectDamage() {
+    public void test_Swashbuckler_Increased_Level_Correct_Damage() {
         Hero swashbuckler = new Swashbuckler("Emre Swashbuckler");
         double expectedDamage = 1.1;
 
@@ -45,7 +61,7 @@ public class SwashbucklerTest {
     }
 
     @Test
-    public void testSwashbucklerIncreasedLevelCorrectAttributes() {
+    public void test_Swashbuckler_Increased_Level_Strength_Correct_Attribute() {
         Hero swashbuckler = new Swashbuckler("Emre Archer");
         swashbuckler.levelUp();
 
@@ -53,7 +69,27 @@ public class SwashbucklerTest {
         HeroAttribute expectedAttributes = new HeroAttribute(3, 10, 2);
 
         assertEquals(expectedAttributes.getStrength(), actualAttributes.getStrength());
+    }
+
+    @Test
+    public void test_Swashbuckler_Increased_Level_Dexterity_Correct_Attribute() {
+        Hero swashbuckler = new Swashbuckler("Emre Archer");
+        swashbuckler.levelUp();
+
+        HeroAttribute actualAttributes = swashbuckler.totalAttributes();
+        HeroAttribute expectedAttributes = new HeroAttribute(3, 10, 2);
+
         assertEquals(expectedAttributes.getDexterity(), actualAttributes.getDexterity());
+    }
+
+    @Test
+    public void test_Swashbuckler_Increased_Level_Intelligence_Correct_Attribute() {
+        Hero swashbuckler = new Swashbuckler("Emre Archer");
+        swashbuckler.levelUp();
+
+        HeroAttribute actualAttributes = swashbuckler.totalAttributes();
+        HeroAttribute expectedAttributes = new HeroAttribute(3, 10, 2);
+
         assertEquals(expectedAttributes.getIntelligence(), actualAttributes.getIntelligence());
     }
 
