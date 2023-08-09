b4f1df46804a4f6a273c01742e87633408981c32
98emre <emre-98@live.se>
2023-08-09 16:18:43 +0200

    fix: made each method for each assert and easier to read the names for the method

--- assignment1/src/test/java/items/WeaponTest.java
+++ assignment1/src/test/java/items/WeaponTest.java
@@ -1,6 +1,7 @@
 package items;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -12,21 +13,47 @@ import app.types.WeaponType;
 public class WeaponTest {
 
     @Test
-    public void testArmorConstructor() {
+    public void test_Weapon_Constructor() {
         Item weapon = new Weapon("TestWeapon", 21, WeaponType.STAFFS, 10);
+        assertTrue(weapon != null);
+    }
 
+    @Test
+    public void test_Weapon_Constructor_Get_Name() {
+        Item weapon = new Weapon("TestWeapon", 21, WeaponType.STAFFS, 10);
         String exceptedName = "TestWeapon";
-        int exceptedRequiredLevel = 21;
-        WeaponType exceptedWeaponType = WeaponType.STAFFS;
-        Slot exceptedSlotType = Slot.WEAPON;
-        int exceptedDamage = 10;
 
         assertEquals(exceptedName, weapon.getName());
-        assertEquals(exceptedRequiredLevel, weapon.getRequiredLevel());
-        assertEquals(exceptedWeaponType, ((Weapon) weapon).getWeaponType());
-        assertEquals(exceptedSlotType, weapon.getSlot());
-        assertEquals(exceptedDamage, ((Weapon) weapon).getWeaponDamage());
+    }
 
+    @Test
+    public void test_Weapon_Constructor_Required_Level() {
+        Item weapon = new Weapon("TestWeapon", 21, WeaponType.STAFFS, 10);
+        int exceptedRequiredLevel = 21;
+
+        assertEquals(exceptedRequiredLevel, weapon.getRequiredLevel());
+    }
+
+    @Test
+    public void test_Weapon_Constructor_Weapon_Type() {
+        Item weapon = new Weapon("TestWeapon", 21, WeaponType.STAFFS, 10);
+        WeaponType exceptedWeaponType = WeaponType.STAFFS;
+
+        assertEquals(exceptedWeaponType, ((Weapon) weapon).getWeaponType());
+    }
+
+    @Test
+    public void test_Weapon_Constructor_Slot_Type() {
+        Item weapon = new Weapon("TestWeapon", 21, WeaponType.STAFFS, 10);
+        Slot exceptedSlotType = Slot.WEAPON;
+        assertEquals(exceptedSlotType, weapon.getSlot());
+    }
+
+    @Test
+    public void test_Weapon_Constructor_Weapon_Damage() {
+        Item weapon = new Weapon("TestWeapon", 21, WeaponType.STAFFS, 10);
+        int exceptedDamage = 10;
+        assertEquals(exceptedDamage, ((Weapon) weapon).getWeaponDamage());
     }
 
 }
