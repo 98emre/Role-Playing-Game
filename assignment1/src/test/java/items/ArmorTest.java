a98a23102210064ec6589c0135085873d29fef5e
98emre <emre-98@live.se>
2023-08-09 16:07:47 +0200

    fix: making ever assert have each method so that we only use 1 assert for each method

--- assignment1/src/test/java/items/ArmorTest.java
+++ assignment1/src/test/java/items/ArmorTest.java
@@ -1,6 +1,7 @@
 package items;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -13,24 +14,66 @@ import app.types.Slot;
 public class ArmorTest {
 
     @Test
-    public void testArmorConstructor() {
+    public void test_Armor_Constructor() {
         Item armor = new Armor("ArmorTest", 1, ArmorType.CLOTH, Slot.BODY, new HeroAttribute(1, 2, 3));
+        assertTrue(armor != null);
+    }
 
-        String exceptedName = "ArmorTest";
-        int exceptedRequiredLevel = 1;
-        ArmorType exceptedArmorType = ArmorType.CLOTH;
-        Slot exceptedSlotType = Slot.BODY;
-        HeroAttribute exceptedAttributes = new HeroAttribute(1, 2, 3);
+    @Test
+    public void test_Armor_Name() {
+        Item armor = new Armor("ArmorTest", 1, ArmorType.CLOTH, Slot.BODY, new HeroAttribute(1, 2, 3));
+        assertEquals("ArmorTest", armor.getName());
+    }
 
-        assertEquals(exceptedName, armor.getName());
-        assertEquals(exceptedRequiredLevel, armor.getRequiredLevel());
-        assertEquals(exceptedArmorType, ((Armor) armor).getArmorType());
-        assertEquals(exceptedSlotType, armor.getSlot());
+    @Test
+    public void test_Armor_RequiredLevel() {
+        Item armor = new Armor("ArmorTest", 1, ArmorType.CLOTH, Slot.BODY, new HeroAttribute(1, 2, 3));
+        assertEquals(1, armor.getRequiredLevel());
+    }
 
-        assertEquals(exceptedAttributes.getStrength(), ((Armor) armor).getArmorAttribute().getStrength());
-        assertEquals(exceptedAttributes.getDexterity(), ((Armor) armor).getArmorAttribute().getDexterity());
-        assertEquals(exceptedAttributes.getIntelligence(), ((Armor) armor).getArmorAttribute().getIntelligence());
+    @Test
+    public void test_Armor_ArmorType() {
+        Item armor = new Armor("ArmorTest", 1, ArmorType.CLOTH, Slot.BODY, new HeroAttribute(1, 2, 3));
+        ArmorType expectedArmorType = ArmorType.CLOTH;
+        ArmorType actualArmorType = ((Armor) armor).getArmorType();
 
+        assertEquals(expectedArmorType, actualArmorType);
+    }
+
+    @Test
+    public void test_Armor_SlotType() {
+        Item armor = new Armor("ArmorTest", 1, ArmorType.CLOTH, Slot.BODY, new HeroAttribute(1, 2, 3));
+        Slot expectedSlotType = Slot.BODY;
+        Slot actualSlotType = armor.getSlot();
+
+        assertEquals(expectedSlotType, actualSlotType);
+    }
+
+    @Test
+    public void test_Armor_Attributes_Strength() {
+        Item armor = new Armor("ArmorTest", 1, ArmorType.CLOTH, Slot.BODY, new HeroAttribute(1, 2, 3));
+        HeroAttribute expectedAttributes = new HeroAttribute(1, 2, 3);
+        HeroAttribute actualAttributes = ((Armor) armor).getArmorAttribute();
+
+        assertEquals(expectedAttributes.getStrength(), actualAttributes.getStrength());
+    }
+
+    @Test
+    public void test_Armor_Attributes_Dexterity() {
+        Item armor = new Armor("ArmorTest", 1, ArmorType.CLOTH, Slot.BODY, new HeroAttribute(1, 2, 3));
+        HeroAttribute expectedAttributes = new HeroAttribute(1, 2, 3);
+        HeroAttribute actualAttributes = ((Armor) armor).getArmorAttribute();
+
+        assertEquals(expectedAttributes.getDexterity(), actualAttributes.getDexterity());
+    }
+
+    @Test
+    public void test_Armor_Attributes_Intelligence() {
+        Item armor = new Armor("ArmorTest", 1, ArmorType.CLOTH, Slot.BODY, new HeroAttribute(1, 2, 3));
+        HeroAttribute expectedAttributes = new HeroAttribute(1, 2, 3);
+        HeroAttribute actualAttributes = ((Armor) armor).getArmorAttribute();
+
+        assertEquals(expectedAttributes.getIntelligence(), actualAttributes.getIntelligence());
     }
 
 }
